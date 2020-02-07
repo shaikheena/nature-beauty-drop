ecd802c526f1a8e129c9619db04e5fef2bdd7bc3
Heena Shaik <shaikheena579@gmail.com>
2020-02-07 18:49:06 +0530

    Header removed

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,7 +16,7 @@ const Layout = ({ children }) => {
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
       </Helmet>
-      <Header/>
+      {/* <Header/> */}
       <div className="container-fluid">
         <main>{children}</main>
         <footer>
