d732c83529228a3f27ddfc39ba52472168f1e921
Heena Shaik <shaikheena579@gmail.com>
2020-02-09 10:51:44 +0530

    about.js

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,13 +1,12 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 const logo = require("../images/logo.png")
 var sidebarMenu = [
-    {
-        home: "Home",
-        products: "Products",
-        about: "About Us",
-        contact: "Contact Us"
-    }
+    { label: 'Home', link: '/' },
+    { label: 'Products', link: '/products' },
+    { label: 'About Us', link: '/about' },
+    { label: 'Contact Us', link: '/contact' },
 ];
 
 export default function Sidebar({ }) {
@@ -16,7 +15,9 @@ export default function Sidebar({ }) {
             <img src={logo} className="sidebar__logo mb-4" />
         </div>
         <div className="pl-4 " style={sidebarStyle.menu}>
-            {sidebarMenu.map((menu, index) => <SideMenu key={index} menu={menu} />)}
+            <ul className="list-unstyled">
+                {sidebarMenu.map((menu, index) => <SideMenu key={index} menu={menu} />)}
+            </ul>
         </div>
         <ul class="list-unstyled d-flex justify-content-center mt-5">
             <li class=" p-2"><a href="#"><i class="fas fa-dove"></i></a></li>
@@ -27,25 +28,24 @@ export default function Sidebar({ }) {
 }
 
 const SideMenu = (props) => {
-    var sideMenu = props.menu;
-    return <ul className="list-unstyled">
-        <li><a href="#" className="nav-bar">{sideMenu.home}</a></li>
-        <li><a href="#">{sideMenu.products}</a></li>
-        <li><a href="#">{sideMenu.about}</a></li>
-        <li><a href="#">{sideMenu.contact}</a></li>
-    </ul>
+    var menu = props.menu;
+    return <li>
+        <Link to={menu.link} className="nav-link">{menu.label} </Link>
+    </li>
+
 }
 
 const sidebarStyle = {
     sidebar: {
         "min-width": "20vw",
         "maxWidth": "240px",
+        "min-height": "100vh",
         "background-color": "rgba(255, 255, 255, 1)",
         "flex-direction": "column",
     },
 
     menu: {
-        "flex":1
+        "flex": 1
     }
 
 }
