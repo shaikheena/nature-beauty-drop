25db5407c6ca4fabcb0b8a490b26e9d3e8a3f2c4
Heena Shaik <shaikheena579@gmail.com>
2020-02-10 12:52:49 +0530

    about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Layout from '../components/layout'
 var about_Nbd = [
-    "Nature’s Beauty Drop (NBD) was created to provide natural Organic handmade & herbal products that are not only beautiful but gentle on the environment. Our products are handmade with all natural ingredients that everyone can use: Men, Women,Boys,Girls & including babies.",
+    "Nature’s Beauty Drop (NBD) was created to provide natural Organic handmade & herbal products that are not only beautiful but gentle on the environment. Our products are handmade with all natural ingredients that everyone can use: Men, Women, Boys, Girls & including babies.",
     "Our luxurious ,natural products are 100 % vegan and cruelty free.Hand crafted in small batches with  organic oils , botanicals , pure essential oils, rare clays. Inspired by the magic and beauty of nature.Each product is unique and artistically crafted with love for cosmetics that's good for you and the planet.",
-    "My passion for health and my intimate love of nature has inspired me to creat handmade soaps , body & skin ,Hair care the old fashion way . I once read that we should not put anything on our skin that we would not eat and I’ve applied that motto into my life passion, creating all natural products . I am dedicated to crafting fresh products that have nourishing and healing qualities that literally feed your skin using only the best ingredients that nature has to offer.we use oils known for their nourishing qualities such as olive oil, almond oil, coconut oil, castor oil, Cocoa butter, sheabutter, Avocado oil and many more."
+    "My passion for health and my intimate love of nature has inspired me to creat handmade soaps, body & skin ,Hair care the old fashion way . I once read that we should not put anything on our skin that we would not eat and I’ve applied that motto into my life passion, creating all natural products . I am dedicated to crafting fresh products that have nourishing and healing qualities that literally feed your skin using only the best ingredients that nature has to offer.we use oils known for their nourishing qualities such as olive oil, almond oil, coconut oil, castor oil, Cocoa butter, sheabutter, Avocado oil and many more."
 
 ];
 var mission_Benefit = [
