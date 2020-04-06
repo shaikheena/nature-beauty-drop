c51b0821a03774909682b4f0ab0e7d696b30d9bc
Heena Shaik <shaikheena579@gmail.com>
2020-04-06 18:00:55 +0530

    about.css

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Title from './title'
 
 var about_Nbd = [
     "Nature’s Beauty Drop (NBD) was created to provide natural Organic handmade & herbal products that are not only beautiful but gentle on the environment. Our products are handmade with all natural ingredients that everyone can use: Men, Women, Boys, Girls & including babies.",
-    "Our luxurious ,natural products are 100 % vegan and cruelty free.Hand crafted in small batches with  organic oils , botanicals , pure essential oils, rare clays. Inspired by the magic and beauty of nature.Each product is unique and artistically crafted with love for cosmetics that's good for you and the planet.",
+    "Our luxurious ,natural products are 100% vegan and cruelty free.Hand crafted in small batches with  organic oils , botanicals , pure essential oils, rare clays. Inspired by the magic and beauty of nature.Each product is unique and artistically crafted with love for cosmetics that's good for you and the planet.",
     "My passion for health and my intimate love of nature has inspired me to creat handmade soaps, body & skin ,Hair care the old fashion way . I once read that we should not put anything on our skin that we would not eat and I’ve applied that motto into my life passion, creating all natural products . I am dedicated to crafting fresh products that have nourishing and healing qualities that literally feed your skin using only the best ingredients that nature has to offer.we use oils known for their nourishing qualities such as olive oil, almond oil, coconut oil, castor oil, Cocoa butter, sheabutter, Avocado oil and many more."
 
 ];
@@ -23,7 +23,7 @@ var benefit__Points = [
 export default (props) => {
     return <Layout>
         <Title title={"About"}/>
-        <div className="h-100 p-md-5 bg-light">
+        <div className="h-100 p-md-5 about_screen bg-light">
             <h2 className="about_title">Who We Are ?</h2>
             {about_Nbd.map((value, index) => (
                 <p className="text-secondary  about_para" key={index}>{value}</p>
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Title from './title'
 const product_Img = require("../images/product.jpg")
 export default () => {
     return <Layout>
-        <div className="row ml-3 mt-3">
-            <div className="col-4">
-                <div class=" text-center py-2 border shadow  mb-2 ">
-                    <img src={product_Img} alt="image-1 " class="img__intro__product " />
-                    <div class="mt-4">
-                        <h3 class="Cosmetic__soap "> <a href="#" class="nav-var text-dark">Facial Gels</a>
-                        </h3>
-                        <p>₹50 <span class="text-secondary ml-2"><del> ₹70</del></span></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Title title={"Products"} />
+        
     </Layout>
 }
